8f47a019d5bcb7a11cd87255c842ab18c34e31e7
Mark Anderson <mtandereson64@gmail.com>
2026-09-19 17:05:00 -0500

    app upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './css/App.css'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import {Routes, Route} from "react-router-dom"
+import { MovieProvider } from './contexts/MovieContext'
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar/>
+      <main className="main-content"> 
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favorites" element={<Favorites />}/>
+        </Routes>
+      </main>
+    </MovieProvider>
+  )
+}
+
+export default App
