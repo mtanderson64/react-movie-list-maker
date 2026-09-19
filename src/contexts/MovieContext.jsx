39c59b83a2faf9ed5574ae1eb988d61343e3b656
Mark Anderson <mtandereson64@gmail.com>
2026-09-19 17:19:22 -0500

    favorites page updated

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState, useContext, useEffect } from "react";
 
 const MovieContext = createContext()
 
-export const useMovieContext = () => useContext()
+export const useMovieContext = () => useContext(MovieContext)
 
 export const MovieProvider = ({children}) => {
   const [favorites, setFavorites] = useState([])
