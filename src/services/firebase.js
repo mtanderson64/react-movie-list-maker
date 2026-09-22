94e044b8e123d36b886ad21a960ecfc76ba583a5
Mark Anderson <mtandereson64@gmail.com>
2026-09-22 06:15:40 -0500

    auth update

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -17,6 +17,10 @@ export const auth = getAuth(app);
 
 export const googleProvider = new GoogleAuthProvider();
 
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
 export const signInWithGoogle = () => {
   return signInWithPopup(auth, googleProvider);
 };
